0b62d3c836e5e1880cd5110a773be3e02d72ac51
malaksati <malaksati999@gmail.com>
2024-09-17 21:40:52 +0300

    Final Commit

--- app/Http/Controllers/auth/RegisterController.php
+++ app/Http/Controllers/auth/RegisterController.php
@@ -12,10 +12,24 @@ class RegisterController extends Controller
     function register() {
         return view("auth.register");
     }
-    function handle_register(Request $req) {
-        $data = $req->all();
-        $data['password']= Hash::make($data['password']);
-        User::create($data);
+    function handle_register(Request $request) {
+        $user=new User();
+        $user->name = $request->name;
+        $user->email=$request->email;
+        $pass= Hash::make($request->password);
+        $user->password=$pass;
+        $imgHolder = "";
+        if($request->hasFile('image')){
+            $image = $request->file('image');
+            $name = time() . "_user_" . "." . $image->getClientOriginalExtension();
+            $destination = public_path("/uploads");
+            $image->move($destination, $name);
+            $imgHolder = "uploads/{$name}";
+            $user['image'] = $imgHolder;
+        }else {
+            $user['image'] = "uploads/default-profile.jpg";
+        }
+        $user->save();
         return redirect('/login');
     }
 }
